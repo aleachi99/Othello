109b88f9b44569dbd8f60a5b83ab140383a61683
AlessandroAchille <AlessandroAchille@AlessandroPc>
2017-08-16 10:40:35 +0200

    Creata classe Casella

---
+++ Othello/src/model/Casella.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author AlessandroAchille
+ */
+public class Casella {
+    boolean libero;
+    String Colore;
+    int riga;
+    int colonna;
+    public Casella (int r, int c){
+        libero = true;
+        Colore = " ";
+    }
+    public Casella (int r, int c, String col){ 
+        libero = false;
+        Colore = col;
+    }
+    
+    public void cambiaColore (String col){
+        Colore = col;
+        libero=false;
+    }
+  
+    public boolean casellaVuota(){
+        return libero;
+    }
+    @Override
+    public String toString (){
+        return Colore;
+    }
+}
